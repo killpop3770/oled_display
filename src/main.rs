5bc331edda0a1d442876878ae6f1c8f9cadc7923
dog <komstroj3@mail.ru>
2025-01-16 16:19:29 +0300

    downgrade ssd1306 version

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,50 @@
 #![no_main]
 
 use cortex_m_rt::entry;
+use embedded_graphics::Drawable;
 use embedded_graphics::{image::{Image, ImageRaw}, pixelcolor::BinaryColor, prelude::Point};
 use panic_halt as _;
-use embedded_graphics::Drawable;
-use ssd1306::{prelude::*, Ssd1306};
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 use stm32f1xx_hal::{
-    i2c::{BlockingI2c, DutyCycle, Mode}, prelude::*, stm32
+    i2c::{BlockingI2c, DutyCycle, Mode},
+    prelude::*,
+    stm32,
 };
+
+// struct I2CInterfaceWrapper(OLED);
+// type OLED = BlockingI2c<
+//     I2C1,
+//     (
+//         Pin<'B', 8, Alternate<OpenDrain>>,
+//         Pin<'B', 9, Alternate<OpenDrain>>,
+//     ),
+// >;
+// #[derive(PartialEq, Eq, Clone, Debug, Copy)]
+// struct I2CError {}
+// impl i2c::Error for I2CError {
+//     fn kind(&self) -> i2c::ErrorKind {
+//         i2c::ErrorKind::Other
+//     }
+// }
+// impl ErrorType for I2CInterfaceWrapper {
+//     type Error = I2CError;
+// }
+// impl I2c for I2CInterfaceWrapper {
+//     fn transaction(
+//         &mut self,
+//         _address: u8,
+//         _operations: &mut [Operation<'_>],
+//     ) -> Result<(), Self::Error> {
+//         Ok(())
+//     }
+// }
 
 #[entry]
 fn main() -> ! {
     let dp = stm32::Peripherals::take().unwrap();
 
     let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
+    let rcc = dp.RCC.constrain();
 
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
@@ -41,25 +71,16 @@ fn main() -> ! {
         1000,
     );
 
-    let interface = I2CInterface::new(i2c, 0, 0);
-    
+    // use ssd1306::test_helpers::I2cStub;
+    let interface = I2CDisplayInterface::new(i2c);
     let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
         .into_buffered_graphics_mode();
 
     display.init().unwrap();
-
     let raw: ImageRaw<BinaryColor> = ImageRaw::new(include_bytes!("./rust.raw"), 64);
-
     let im = Image::new(&raw, Point::new(32, 0));
-
     im.draw(&mut display).unwrap();
-
     display.flush().unwrap();
 
     loop {}
 }
-
-// #[exception]
-// fn HardFault(ef: &ExceptionFrame) -> ! {
-//     panic!("{:#?}", ef);
-// }
